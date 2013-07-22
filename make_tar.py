1d319229621d6e0d4376aea687a6349eb239bab6
jacobb <jgboerema@gmail.com>
2013-07-22 20:37:14 +0200

    Dev: Update our AI version number in script that makes release tar.

--- make_tar.py
+++ make_tar.py
@@ -12,19 +12,20 @@ ai_name = "WormAI"
 ai_pack_name = ai_name.replace(" ", "-")
 
 # ----------------------------------
-
+version_file = "version.nut";
 
 # Script:
 version = -1
-for line in open("version.nut"):
-
-	r = re.search('SELF_VERSION\s+<-\s+([0-9]+)', line)
-	if(r != None):
-		version = r.group(1)
+found_line = 0
+for line in open(version_file):
+    found_line += 1;
+    r = re.search('SELF_VERSION\s+<-\s+([0-9]+)', line)
+    if (r != None):
+        version = r.group(1)
 
 if(version == -1):
-	print("Couldn't find " + ai_name + " version in info.nut!")
-	exit(-1)
+    print("Couldn't find " + ai_name + " version in info.nut!")
+    exit(-1)
 
 base_dir_name = ai_pack_name + "-v" + version
 temp_dir_name = "..\\temp"
@@ -48,3 +49,27 @@ os.system("run_tar.bat " + tar_name + " " + base_dir_name)
 # Now copy it to our WormAI\releases folder...
 os.system("xcopy " + temp_dir_name + "\\" + tar_name + " releases\\");
 os.system("del " + temp_dir_name + "\\" + tar_name)
+
+
+# Finally we want to update our version number
+# Can't open it both for reading and writing, or need different parameter, not bothering to 
+# look into it now just open it twice
+version_nut = open(version_file);
+lines = version_nut.readlines();
+version_nut.close();
+
+#Now open for writing
+version_nut = open(version_file, "wt");
+
+# Write all lines
+for i, line in enumerate(lines):
+    if found_line == i+1:
+        # Replace string version number with increased version number
+        version = int(version) + 1;
+        version_nut.write("SELF_VERSION <- {0};\n" .format(version));
+    else:
+        # Rewrite the line
+        version_nut.write(line);
+#    print(str(i) + "  " + line);
+
+version_nut.close();
