f9737c0c4abfbc7a679a0b3db6baac95b3012187
Jacob Boerema <jgboerema@gmail.com>
2015-06-05 21:04:35 +0200

    Dev: Add script to switch between normal and dev version AI names.

---
+++ dev/set_ai_names.py
@@ -0,0 +1,65 @@
+#!python
+
+# Replace AI names with some other names
+# Useful to have different names for developing and then being
+# able to switch names for releases.
+
+import os
+import io
+import re
+
+# ----------------------------------
+# Definitions:
+# ----------------------------------
+
+# AI names
+# AI Release version names
+ai_name_short = "WORM"
+ai_name_long  = "WormAI"
+# AI Development version names
+ai_dev_name_short = "WOR0"
+ai_dev_name_long  = "WormAIDev"
+
+# ----------------------------------
+# The information we are changing is in info.nut
+info_nut_file = "info.nut";
+
+# We want the date to be updated before releasing the tar
+# and the version number to be update after updating
+# Thus we can't do it at the same time.
+quoted_name = ""
+replace_name = ""
+
+# Open file for reading and writing (r+)
+info_file = io.open(info_nut_file, "r+", newline='')   # newline='' means don't convert line endings
+lines = info_file.readlines();
+info_file.seek(0);
+
+for i, line in enumerate(lines):
+    # Find the lines where a text string is returned
+    r = re.search('return\s+\"([a-zA-Z0-9]+)\"', line)
+    if (r != None):
+        quoted_name = r.group(1);
+        # Check if the quoted name is one of the names we're looking for
+        do_replace = True;
+        if quoted_name == ai_name_short:
+            replace_name = ai_dev_name_short;
+        elif quoted_name == ai_name_long:
+            replace_name = ai_dev_name_long;
+        elif quoted_name == ai_dev_name_short:
+            replace_name = ai_name_short;
+        elif quoted_name == ai_dev_name_long:
+            replace_name = ai_name_long;
+        else:
+            do_replace = False;
+            replace_name = "";
+        if (do_replace):
+            print "Replacing: '" +  quoted_name + "' with: '" + replace_name + "'";
+            line = line[:r.start(1)] + replace_name + line[r.end(1):];
+    # Write the possibly changed line back to file
+    info_file.write(line);
+
+# Truncate file to current size since it may have been longer before
+info_file.truncate();
+# Close our file
+info_file.close();
